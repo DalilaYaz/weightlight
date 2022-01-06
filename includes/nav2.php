4f4b8e9a6ff778a5483790c26da7e46375a4f93a
Chaimaa Lougsari <lougsari@outlook.com>
2022-01-06 15:18:49 +0100

    commit

---
+++ includes/nav2.php
@@ -0,0 +1,10 @@
+<header class="header">
+
+    <div class="line"></div>
+    <div class="row rowNav">
+        <h1 class="mainLogo"><span class="green">Weight</span><span class="orange">Light</span></h1>
+
+
+
+    </div>
+</header>
--- signup.php
+++ signup.php
@@ -1,15 +1,19 @@
+<?php require_once 'includes/nav1.php' ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/utilities.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/utilities.css">
 </head>
+
 <body>
-    <p>Bienvenue sur <h2 class="mainLogo"><span id="green">Weight</span><span id="orange">Light</span></h2>, Inscrivez vous.</p>
+    <p>Bienvenue sur
+    <h2 class="mainLogo"><span id="green">Weight</span><span id="orange">Light</span></h2>, Inscrivez vous.</p>
     <div id="conteneurSign">
         <div id="formSign">
 
