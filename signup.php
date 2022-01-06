707bcd7a9766a29f41ec3a893336188ab3c55240
Thomas Plantefeve <84777732+ThomPlan173@users.noreply.github.com>
2022-01-06 14:57:32 +0100

    modif CSS

---
+++ signup.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/utilities.css">
+</head>
+<body>
+    <p>Bienvenue sur <h2 class="mainLogo"><span id="green">Weight</span><span id="orange">Light</span></h2>, Inscrivez vous.</p>
+    <div id="conteneurSign">
+        <div id="formSign">
+
+            <label for=""><b>Prenom</b></label><br>
+            <input type="text" name="" id=""><br>
+            <label for=""><b>Nom</b></label><br>
+            <input type="text" name="" id=""><br>
+            <label for=""><b>Email</b></label><br>
+            <input type="text" name="" id=""><br>
+            <label for=""><b>Mot de passe</b></label><br>
+            <input type="text" name="" id=""><br>
+
+            <label for=""><b>Sexe</b></label><br>
+            <input type="radio" name="Sexe" id="SexeChoice1">
+            <label for="SexeChoice1">Féminin</label>
+            <input type="radio" name="Sexe" id="SexeChoice2">
+            <label for="SexeChoice2">Masculin</label>
+            <input type="radio" name="Sexe" id="SexeChoice3">
+            <label for="SexeChoice3">Autre</label> <br>
+
+            <label for=""><b>Poids</b></label><br>
+            <input type="text" name="" id=""><br>
+            <label for=""><b>Taille</b></label><br>
+            <input type="text" name="" id=""><br>
+            <label for=""><b>Date de naissance</b></label><br>
+            <input type="text" name="" id=""><br>
+            <input type="button" value="Créer en compte"><br>
+
+            <p>Déjà inscrit ?</p><a href="./login.php">Connectez-vous</a><br>
+        </div>
+        <div class="flex">
+            <img id="signImg" src="./images/home/diet.png" alt="" srcset="">
+        </div>
+    </div>
+</body>
+
+</html>
