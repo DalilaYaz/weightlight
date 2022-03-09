88d7ecad27e31e617e56b725a3d3eb2762db6b20
Chaimaa Lougsari <lougsari@outlook.com>
2022-03-09 14:52:08 +0100

    update

--- js/details.js
+++ js/details.js
@@ -23,11 +23,12 @@ let myChart = new Chart(graph, {
     options:{
       responsive: true,
       maintainAspectRatio: false,
+      
       plugins: {
         legend: {
           display: true,
           position: 'right',
-          fontSize : 50,
+         
         }
       },
       title:{
