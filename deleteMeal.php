395107c2d1a569334973e3be2b426f8fd809c4fa
Thomas Plantefeve <84777732+ThomPlan173@users.noreply.github.com>
2022-04-06 12:57:02 +0200

    delete meal fonctionnel

--- deleteMeal.php
+++ deleteMeal.php
@@ -1,20 +1,27 @@
 <?php
 
-include "database.php";
+include('database.php');
 
-if (isset($_GET['idRepas '])) {
+if (isset($_GET['idRepas'])) {
 
-    $idRepas  = $_GET['idRepas '];
+    $idRepas  = $_GET['idRepas'];
 
-    $sql = "DELETE FROM `repas` WHERE `idRepas `='$idRepas '";
+    $sql = "DELETE FROM consommer WHERE consommer.idRepas=$idRepas";
+    $result = mysqli_query($db, $sql);
+    var_dump($result);
+    $sql2 = "DELETE FROM repas WHERE repas.idRepas=$idRepas";
+    $result2 = mysqli_query($db, $sql2);
+    var_dump($result);
 
-    $result = $db->query($sql);
+    if ($result == true && $result2 == true) {
+        echo '<script>alert("Repas supprimé avec succès")</script>';
+    }
 
-    if ($result == TRUE) {
-
-        echo "Record deleted successfully.";
+    /*if ($result == TRUE && $result2 == TRUE) {
+        echo " deleted successfully.";
     } else {
 
         echo "Error:" . $sql . "<br>" . $db->error;
-    }
+        echo "Error:" . $sql2 . "<br>" . $db->error;
+    }*/
 }
